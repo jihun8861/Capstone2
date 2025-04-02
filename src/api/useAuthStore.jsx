10b645a6b317ee6d6425bd0a460772e1d4685a79
Yeom1009 <yjg5641@oasis.inje.ac.kr>
2025-04-02 15:11:26 +0900

    250402 (2)

--- src/api/useAuthStore.jsx
+++ src/api/useAuthStore.jsx
@@ -1,28 +1,28 @@
-import { create } from 'zustand';
-import axios from 'axios';
+import { create } from "zustand";
+import axios from "axios";
 
 export const useAuthStore = create((set) => ({
-  user: JSON.parse(localStorage.getItem('user')) || null,
+  user: JSON.parse(localStorage.getItem("user")) || null,
   loading: true,
-  
+
   fetchUserData: async () => {
     const token = localStorage.getItem("token");
     if (!token) {
       set({ loading: false });
       return;
     }
-  
+
     set({ loading: true });
-  
+
     try {
       const response = await axios.post(
         "https://port-0-edcustom-lxx6l4ha4fc09fa0.sel5.cloudtype.app/findbodybytoken",
         { token }
       );
-  
+
       if (response.data && response.data.data) {
         const userData = JSON.parse(response.data.data); // JSON 문자열을 객체로 변환
-        localStorage.setItem('user', JSON.stringify(userData));
+        localStorage.setItem("user", JSON.stringify(userData));
         set({ user: userData, loading: false });
       } else {
         console.error("유효하지 않은 응답 데이터:", response.data);
@@ -37,10 +37,9 @@ export const useAuthStore = create((set) => ({
       set({ user: null, loading: false });
     }
   },
-  
-  
+
   setUser: (userData) => {
-    localStorage.setItem('user', JSON.stringify(userData));
+    localStorage.setItem("user", JSON.stringify(userData));
     set({ user: userData });
   },
 }));
